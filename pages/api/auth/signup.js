0770a99fcf8501e811e2d7350a62b91d8b71acb0
Dwi Kuntarto <dwikuntarto.work@gmail.com>
2021-10-04 14:23:04 +0700

    first commit

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,39 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+    if (req.method === 'POST') {
+        const data = req.body;
+
+        const { email, password } = data;
+
+        if (!email || !email.includes('@') || !password || password.trim().length < 7) {
+            res.status(422).json({ message: 'Invalid input - password should be at least 7 character long.' })
+            return;
+        }
+
+        const client = await connectToDatabase();
+
+        const db = client.db();
+
+        const existingUser = await db.collection('users').findOne({ email: email });
+
+        if (existingUser) {
+            res.status(422).json({ message: 'User exists already!' });
+            client.close();
+            return;
+        }
+
+        const hashedPassword = await hashPassword(password);
+
+        const result = await db.collection('users').insertOne({
+            email: email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({ message: 'Created User!' });
+        client.close();
+    }
+}
+
+export default handler;
